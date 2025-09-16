1ff58357418b86b13adb1746abef876195229ce6
Beldendy <ruthmetayer0@gmail.com>
2025-09-16 10:16:57 +0200

    feat: commencement d'une page d'acceuil

--- view/accueil.php
+++ view/accueil.php
@@ -1,1 +1,53 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <title>LPRS</title>
+    </head>
+    <body>
+        <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+        </script>
+        <header>
+            <menu class="nav">
+                <li>
+                    <a class="navbar-brand" href="accueil.php">Acceuil</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Se connecter/S'inscrire
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="connexion.php">Connexion</a></li>
+                        <li><a class="dropdown-item" href="inscription.php">Inscription</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Evenements/Actualité
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Tous les evenement</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Annuaire
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Anciens eleves</a></li>
+                    </ul>
+                </li>
+            </menu>
+        </header>
+        <section class="banner">
+            <h1>Bienvenue</h1>
+        </section>
+
+    </body>
+</html>
