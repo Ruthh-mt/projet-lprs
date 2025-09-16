1a1d752c111a9c650e246bfb3c5c000be063c09b
Beldendy <ruthmetayer0@gmail.com>
2025-09-16 10:54:49 +0200

    feat: ajout des modele etudient et prof

---
+++ src/modele/ProfModel.php
@@ -0,0 +1,55 @@
+<?php
+class ProfModel extends userModel{
+    private $specialite;
+    private$ref_utilisateur;
+    public function __construct(PDO $db, array $donnees){
+        parent::__construct($db);
+        $this->hydrate($donnees);
+
+    }
+    public function hydrate(array $donnees){
+        foreach ($donnees as $key => $value) {
+            $method = 'set'.ucfirst($key);
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSpecialite()
+    {
+        return $this->specialite;
+    }
+
+    /**
+     * @param mixed $specialite
+     */
+    public function setSpecialite($specialite): void
+    {
+        $this->specialite = $specialite;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRefUtilisateur()
+    {
+        return $this->ref_utilisateur;
+    }
+
+    /**
+     * @param mixed $ref_utilisateur
+     */
+    public function setRefUtilisateur($ref_utilisateur): void
+    {
+        $this->ref_utilisateur = $ref_utilisateur;
+    }
+
+
+
+
+
+}
