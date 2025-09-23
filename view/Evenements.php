0bdfe8b7938b05234bcd1aff052b5ad2801541f7
Augustin-Erceville <a.erceville@lprs.fr>
2025-09-23 10:17:50 +0200

    feat • Rédaction de la page Evenements.php (sans PHP)

--- view/Evenements.php
+++ view/Evenements.php
@@ -34,3 +34,131 @@
           <?php endif; ?>
      </div>
 </header>
+<section class="container">
+     <div class="d-grid gap-2">
+          <button class="btn btn-outline-success text-uppercase my-3" type="button">Ajouter un évènement</button>
+     </div>
+     <article class="row my-3">
+          <div class="card-group">
+               <div class="card">
+                    <img src="https://wallpapers.com/images/hd/4k-vector-snowy-landscape-p7u7m7qyxich2h31.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                         <h5 class="card-title">Card title</h5>
+                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    <div class="card-footer">
+                         <small class="text-body-secondary">Last updated 3 mins ago</small>
+                    </div>
+               </div>
+               <div class="card">
+                    <img src="https://wallpapers.com/images/hd/4k-vector-japan-landscape-91fou43c3r7lglnr.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                         <h5 class="card-title">Card title</h5>
+                         <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                    <div class="card-footer">
+                         <small class="text-body-secondary">Last updated 3 mins ago</small>
+                    </div>
+               </div>
+               <div class="card">
+                    <img src="https://i.redd.it/4syggbto2km01.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                         <h5 class="card-title">Card title</h5>
+                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                    </div>
+                    <div class="card-footer">
+                         <small class="text-body-secondary">Last updated 3 mins ago</small>
+                    </div>
+               </div>
+          </div>
+     </article>
+</section>
+<section class="container">
+     <article class="row my-3">
+          <div class="card-group">
+               <div class="card">
+                    <img src="https://i.redd.it/4syggbto2km01.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                         <h5 class="card-title">Card title</h5>
+                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    <div class="card-footer">
+                         <small class="text-body-secondary">Last updated 3 mins ago</small>
+                    </div>
+               </div>
+               <div class="card">
+                    <img src="https://wallpapers.com/images/hd/4k-vector-snowy-landscape-p7u7m7qyxich2h31.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                         <h5 class="card-title">Card title</h5>
+                         <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                    <div class="card-footer">
+                         <small class="text-body-secondary">Last updated 3 mins ago</small>
+                    </div>
+               </div>
+               <div class="card">
+                    <img src="https://wallpapers.com/images/hd/4k-vector-japan-landscape-91fou43c3r7lglnr.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                         <h5 class="card-title">Card title</h5>
+                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                    </div>
+                    <div class="card-footer">
+                         <small class="text-body-secondary">Last updated 3 mins ago</small>
+                    </div>
+               </div>
+          </div>
+     </article>
+</section>
+<section class="container">
+     <article class="row my-3">
+          <div class="card-group">
+               <div class="card">
+                    <img src="https://wallpapers.com/images/hd/4k-vector-snowy-landscape-p7u7m7qyxich2h31.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                         <h5 class="card-title">Card title</h5>
+                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    <div class="card-footer">
+                         <small class="text-body-secondary">Last updated 3 mins ago</small>
+                    </div>
+               </div>
+               <div class="card">
+                    <img src="https://i.redd.it/4syggbto2km01.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                         <h5 class="card-title">Card title</h5>
+                         <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                    <div class="card-footer">
+                         <small class="text-body-secondary">Last updated 3 mins ago</small>
+                    </div>
+               </div>
+               <div class="card">
+                    <img src="https://wallpapers.com/images/hd/4k-vector-japan-landscape-91fou43c3r7lglnr.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                         <h5 class="card-title">Card title</h5>
+                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                    </div>
+                    <div class="card-footer">
+                         <small class="text-body-secondary">Last updated 3 mins ago</small>
+                    </div>
+               </div>
+          </div>
+     </article>
+     <nav aria-label="Page navigation example">
+          <ul class="pagination justify-content-center">
+               <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Previous">
+                         <span aria-hidden="true">&laquo;</span>
+                    </a>
+               </li>
+               <li class="page-item"><a class="page-link" href="#">1</a></li>
+               <li class="page-item"><a class="page-link" href="#">2</a></li>
+               <li class="page-item"><a class="page-link" href="#">3</a></li>
+               <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next">
+                         <span aria-hidden="true">&raquo;</span>
+                    </a>
+               </li>
+          </ul>
+     </nav>
+</section>
--- view/accueil.php
+++ view/accueil.php
@@ -4,7 +4,7 @@
 <head>
      <meta charset="utf-8">
      <meta name="viewport" content="width=device-width, initial-scale=1">
-     <title>INSCRIPTION • LPRS</title>
+     <title>ACCUEIL • LPRS</title>
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
@@ -15,16 +15,15 @@
           <a href="accueil.php" class="d-inline-flex link-body-emphasis text-decoration-none">
                <img src="https://media.tenor.com/1DV7nkfj5OkAAAAM/blobgodeto-blobdance.gif" class="rounded-circle mx-3"
                     style="max-width: 15%; height: auto;">
-               <div class="fs-4 text-light">LPRS</div>
+               <div class="fs-4 text-light text-uppercase">LPRS • Accueil</div>
           </a>
      </div>
      <ul class="nav col mb-2 justify-content-center mb-md-0">
-          <li class="nav-item"><a href="accueil.php" class="btn btn-outline-primary dropdown me-2">Accueil</a></li>
-          <li class="nav-item"><a href="#" class="btn btn-outline-light me-2">Évènements</a></li>
-          <li class="nav-item"><a href="#" class="btn btn-outline-light me-2">Annuaire</a></li>
-          <li class="nav-item"><a href="#" class="btn btn-outline-light me-2">Liste des élèves</a></li>
+          <li class="nav-item"><a href="accueil.php" class="btn btn-light dropdown me-2">Accueil</a></li>
+          <li class="nav-item"><a href="Evenements.php" class="btn btn-outline-light me-2">Évènements</a></li>
+          <li class="nav-item"><a href="Annuaire.php" class="btn btn-outline-light me-2">Annuaire</a></li>
+          <li class="nav-item"><a href="ListeEleves.php" class="btn btn-outline-light me-2">Liste des élèves</a></li>
      </ul>
-
      <div class="col-2 btn-group md-3 me-3 text-end" role="group" aria-label="Boutons utilisateur">
           <?php if (isset($_SESSION['utilisateur'])): ?>
                <a href="#" class="btn btn-outline-primary">MON COMPTE</a>
@@ -35,11 +34,11 @@
           <?php endif; ?>
      </div>
 </header>
-<section class="banner">
+<section class="container banner text-center bg-dark text-white text-center py-3 rounded">
      <h1>Bienvenue</h1>
 </section>
 <section class="container border rounded border-dark my-3">
-     <h2 style="text-align: center" class="text-uppercase">Présentation</h2>
+     <h3 class="text-center py-3 text-center text-uppercase">Présentation</h3>
      <article class="article" style="text-align: justify;">
           <p>
                L'école souhaite développer un site web dédié à la gestion des anciens élèves (alumni) et aux relations avec les entreprises.
@@ -49,7 +48,7 @@
      </article>
 </section>
 <section class="container border rounded border-dark my-3">
-     <h2 style="text-align: center" class="text-uppercase">Dernières offres postées</h2>
+     <h3 class="text-center py-3 text-center text-uppercase">Dernières offres postées</h3>
      <article class="article row" style="text-align: justify;">
           <div class="col card m-3" style="width: 18rem;">
                <img src="https://images.hdqwalls.com/wallpapers/mountains-minimalists-4k-vx.jpg" class="card-img-top" alt="...">
@@ -94,7 +93,7 @@
      </article>
 </section>
 <section class="container border rounded border-dark my-3">
-     <h2 style="text-align: center" class="text-uppercase">Derniers évènements créés</h2>
+     <h3 class="text-center py-3 text-center text-uppercase">Derniers évènements créés</h3>
      <article class="article row" style="text-align: justify;">
           <div class="col card m-3" style="width: 18rem;">
                <img src="https://wallpapercave.com/wp/wp3403850.jpg" class="card-img-top" alt="...">
