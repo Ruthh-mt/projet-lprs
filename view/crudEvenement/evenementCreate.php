546deb6462a8f489fb492483fb1af3b5eeab90a9
Beldendy <ruthmetayer0@gmail.com>
2025-10-07 10:21:12 +0200

    feat: Ajout d'une fonction pour ajouter un evenement

--- view/crudEvenement/evenementCreate.php
+++ view/crudEvenement/evenementCreate.php
@@ -1,10 +1,9 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) {
-    // ajouter que seulement un user connecter et qui n'est pas gestionnaire peut se connecter
     session_start();
- }
-?>
+}
 
+?>
 <!doctype html>
 <html lang="fr">
 <head>
@@ -51,6 +50,19 @@ if (session_status() === PHP_SESSION_NONE) {
     </div>
 </header>
 <form action="../../src/treatment/traitementAjoutEvenement.php" method="post">
+    <?php if (isset($_SESSION['error'])): ?>
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <?= $_SESSION['error']; unset($_SESSION['error']); ?>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    <?php endif; ?>
+
+    <?php if (isset($_SESSION['success'])): ?>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <?= $_SESSION['success']; unset($_SESSION['success']); ?>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    <?php endif; ?>
     <label for="titre_eve"> Titre de l'evenement</label>
     <input class="form-control" type="text" id="titre_eve" name="titre_eve" placeholder="Entrez le titre de l'eve">
     <label for="type_eve">Type de l'evenement</label>
@@ -61,9 +73,13 @@ if (session_status() === PHP_SESSION_NONE) {
     <input class="form-control" type="text" id="lieu_eve" name="lieu_eve" placeholder="Entrez le type de l'eve">
     <label for="element_eve">Element pour l'evenement</label>
     <textarea class="form-control" type="text" id="element_eve" name="element_eve" placeholder="Entrez les element requis pour l'eve"></textarea>
-    <label for="nb_plc">Type de l'evenement</label>
+    <label for="nb_plc">Nombre de place disponible </label>
     <input class="form-control" type="number" id="nb_plc" name="nb_plc" placeholder="Entrez le nombres de place disponible">
     <button class="btn btn-outline-primary" type="submit">Valider</button>
 </form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
 
 
