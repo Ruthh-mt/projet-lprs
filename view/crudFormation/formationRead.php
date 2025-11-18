7199969e2a0da67a12e779e1e186f6be16906bbd
Augustin-Erceville <augustinderceville@gmail.com>
2025-11-18 20:22:48 +0100

    feat - Mise à jour du header des admin gestionnaire, admin partenaire, admin formation, admin entreprise.

--- view/crudFormation/formationRead.php
+++ view/crudFormation/formationRead.php
@@ -1,4 +1,5 @@
 <?php
+$prefix = explode('/view/', $_SERVER['HTTP_REFERER'])[0].'/public';
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
@@ -26,13 +27,11 @@ $page = 'Formation';
 </head>
 <body>
 <header
-        class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 border-bottom bg-dark">
+        class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom bg-dark">
     <div class="col-2 ms-3 mb-2 mb-md-0 text-light">
-        <a href="accueil.php" class="d-inline-flex link-body-emphasis text-decoration-none">
-            <img src="https://media.tenor.com/ifEkV-aGn3EAAAAi/fat-cat.gif"
-                 class="rounded-circle mx-3"
-                 style="max-width: 15%; height: auto;">
-            <div class="fs-4 text-light text-uppercase">LPRS • ADMIN</div>
+        <a href="../accueil.php" class="d-inline-flex link-body-emphasis text-decoration-none">
+            <img src="https://i.pinimg.com/originals/a0/50/1e/a0501e0c5659dcfde397299e4234e75a.gif" class="mx-3" style="max-width: 48px;">
+            <div class="fs-4 text-light text-uppercase">LPRS</div>
         </a>
     </div>
     <ul class="nav col mb-2 justify-content-center mb-md-0">
@@ -48,23 +47,42 @@ $page = 'Formation';
             </li>
         <?php endif; ?>
     </ul>
-    <div class="col-2 btn-group md-3 me-3 text-end" role="group" aria-label="Boutons utilisateur">
-        <?php if (isset($_SESSION['utilisateur'])): ?>
-            <a href="../account/accountRead.php" class="btn btn-outline-primary">Mon compte</a>
-            <a href="../src/treatment/traitementDeconnexion.php" class="btn btn-outline-danger">Déconnexion</a>
-        <?php else: ?>
-            <a href="../connexion.php" class="btn btn-outline-success">Connexion</a>
-            <a href="../inscription.php" class="btn btn-outline-primary">Inscription</a>
-        <?php endif; ?>
+    <div class="col-2 text-end me-3">
+        <div class="dropdown">
+            <?php if (isset($_SESSION['utilisateur'])): ?>
+                <?php $avatar = $_SESSION['utilisateur']['avatar'] ?? null; ?>
+                <a href="#" class="d-inline-block text-decoration-none dropdown-toggle"
+                   data-bs-toggle="dropdown" aria-expanded="false">
+                    <?php if ($avatar): ?>
+                        <img src="<?= $prefix.htmlspecialchars($avatar, ENT_QUOTES, 'UTF-8') ?>" alt="Photo de profil" class="rounded-circle" style="max-width: 48px;object-fit:cover;">
+                    <?php else: ?>
+                        <i class="bi bi-person-circle fs-3 text-light"></i>
+                    <?php endif; ?>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end text-small">
+                    <li><a class="dropdown-item text-primary" href="../account/accountRead.php"><i class="bi bi-person"></i> Mon compte</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item text-danger" href="../../src/treatment/traitementDeconnexion.php"><i class="bi bi-box-arrow-right"></i> Déconnexion</a></li>
+                </ul>
+            <?php else: ?>
+                <a href="#" class="d-inline-block text-decoration-none dropdown-toggle"
+                   data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="bi bi-person-circle fs-3 text-light"></i>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end text-small">
+                    <li><a class="dropdown-item text-primary" href="../connexion.php"><i class="bi bi-box-arrow-in-right"></i> Connexion</a></li>
+                    <li><a class="dropdown-item text-success" href="../inscription.php"><i class="bi bi-person-plus"></i> Inscription</a></li>
+                </ul>
+            <?php endif; ?>
+        </div>
     </div>
 </header>
-
 <nav class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom text-white bg-dark">
     <div class="nav col mb-2 justify-content-center mb-md-0">
         <div class="btn-group mx-1" role="group" aria-label="Basic example">
             <a href="../crudEntreprise/entrepriseRead.php" class="btn btn-outline-info">Entreprise</a>
             <a href="../crudEvenement/evenementRead.php" class="btn btn-outline-danger disabled">Évènement</a>
-            <a href="../crudFormation/formationRead.php" class="btn btn-outline-info">Formation</a>
+            <a href="../crudFormation/formationRead.php" class="btn btn-outline-info active">Formation</a>
             <a href="../crudGestionnaire/gestionnaireRead.php" class="btn btn-outline-info">Gestionnaire</a>
             <a href="../crudOffre/offreRead.php" class="btn btn-outline-info">Offre</a>
             <a href="../crudPartenaire/partenaireRead.php" class="btn btn-outline-info">Partenaire</a>
--- view/crudGestionnaire/gestionnaireCreate.php
+++ view/crudGestionnaire/gestionnaireCreate.php
@@ -1,4 +1,5 @@
 <?php
+$prefix = explode('/view/', $_SERVER['HTTP_REFERER'])[0].'/public';
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
@@ -22,36 +23,55 @@ $utilisateurs = $utilisateurRepo->findNonGestionnaires();
           crossorigin="anonymous">
 </head>
 <body>
-<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 border-bottom bg-dark">
+<header
+        class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom bg-dark">
     <div class="col-2 ms-3 mb-2 mb-md-0 text-light">
-        <a href="../../accueil.php" class="d-inline-flex link-body-emphasis text-decoration-none">
-            <img src="https://media.tenor.com/ifEkV-aGn3EAAAAi/fat-cat.gif"
-                 class="rounded-circle mx-3"
-                 style="max-width: 15%; height: auto;">
-            <div class="fs-4 text-light text-uppercase">LPRS • ADMIN</div>
+        <a href="../accueil.php" class="d-inline-flex link-body-emphasis text-decoration-none">
+            <img src="https://i.pinimg.com/originals/a0/50/1e/a0501e0c5659dcfde397299e4234e75a.gif" class="mx-3" style="max-width: 48px;">
+            <div class="fs-4 text-light text-uppercase">LPRS</div>
         </a>
     </div>
     <ul class="nav col mb-2 justify-content-center mb-md-0">
-        <li class="nav-item"><a href="../../accueil.php" class="btn btn-outline-light dropdown me-2">Accueil</a></li>
-        <li class="nav-item"><a href="../../evenements.php" class="btn btn-outline-light me-2">Évènements</a></li>
-        <li class="nav-item"><a href="../../annuaire.php" class="btn btn-outline-light me-2">Annuaire</a></li>
-        <li class="nav-item"><a href="../../listeEleves.php" class="btn btn-outline-light me-2">Liste des élèves</a></li>
-        <li class="nav-item"><a href="../../emplois.php" class="btn btn-outline-light me-2">Emplois</a></li>
-        <li class="nav-item"><a href="../../forum.php" class="btn btn-outline-light me-2">Forum</a></li>
+        <li class="nav-item"><a href="../accueil.php" class="btn btn-outline-light dropdown me-2">Accueil</a></li>
+        <li class="nav-item"><a href="../evenements.php" class="btn btn-outline-light me-2">Évènements</a></li>
+        <li class="nav-item"><a href="../annuaire.php" class="btn btn-outline-light me-2">Annuaire</a></li>
+        <li class="nav-item"><a href="../listeEleves.php" class="btn btn-outline-light me-2">Liste des élèves</a></li>
+        <li class="nav-item"><a href="../emplois.php" class="btn btn-outline-light me-2">Emplois</a></li>
+        <li class="nav-item"><a href="../forum.php" class="btn btn-outline-light me-2">Forum</a></li>
         <?php if (isset($_SESSION['utilisateur']) && $_SESSION['utilisateur']['role'] === 'Gestionnaire'): ?>
             <li class="nav-item">
-                <a href="../../administration.php" class="btn btn-outline-warning active me-2">Administration</a>
+                <a href="../administration.php" class="btn btn-outline-warning active me-2">Administration</a>
             </li>
         <?php endif; ?>
     </ul>
-    <div class="col-2 btn-group md-3 me-3 text-end" role="group" aria-label="Boutons utilisateur">
-        <?php if (isset($_SESSION['utilisateur'])): ?>
-            <a href="../../account/accountRead.php" class="btn btn-outline-primary">Mon compte</a>
-            <a href="../../src/treatment/traitementDeconnexion.php" class="btn btn-outline-danger">Déconnexion</a>
-        <?php else: ?>
-            <a href="../../connexion.php" class="btn btn-outline-success">Connexion</a>
-            <a href="../../inscription.php" class="btn btn-outline-primary">Inscription</a>
-        <?php endif; ?>
+    <div class="col-2 text-end me-3">
+        <div class="dropdown">
+            <?php if (isset($_SESSION['utilisateur'])): ?>
+                <?php $avatar = $_SESSION['utilisateur']['avatar'] ?? null; ?>
+                <a href="#" class="d-inline-block text-decoration-none dropdown-toggle"
+                   data-bs-toggle="dropdown" aria-expanded="false">
+                    <?php if ($avatar): ?>
+                        <img src="<?= $prefix.htmlspecialchars($avatar, ENT_QUOTES, 'UTF-8') ?>" alt="Photo de profil" class="rounded-circle" style="max-width: 48px;object-fit:cover;">
+                    <?php else: ?>
+                        <i class="bi bi-person-circle fs-3 text-light"></i>
+                    <?php endif; ?>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end text-small">
+                    <li><a class="dropdown-item text-primary" href="../account/accountRead.php"><i class="bi bi-person"></i> Mon compte</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item text-danger" href="../../src/treatment/traitementDeconnexion.php"><i class="bi bi-box-arrow-right"></i> Déconnexion</a></li>
+                </ul>
+            <?php else: ?>
+                <a href="#" class="d-inline-block text-decoration-none dropdown-toggle"
+                   data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="bi bi-person-circle fs-3 text-light"></i>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end text-small">
+                    <li><a class="dropdown-item text-primary" href="../connexion.php"><i class="bi bi-box-arrow-in-right"></i> Connexion</a></li>
+                    <li><a class="dropdown-item text-success" href="../inscription.php"><i class="bi bi-person-plus"></i> Inscription</a></li>
+                </ul>
+            <?php endif; ?>
+        </div>
     </div>
 </header>
 <nav class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom text-white bg-dark">
@@ -60,7 +80,7 @@ $utilisateurs = $utilisateurRepo->findNonGestionnaires();
             <a href="../crudEntreprise/entrepriseRead.php" class="btn btn-outline-info">Entreprise</a>
             <a href="../crudEvenement/evenementRead.php" class="btn btn-outline-danger disabled">Évènement</a>
             <a href="../crudFormation/formationRead.php" class="btn btn-outline-info">Formation</a>
-            <a href="../crudGestionnaire/gestionnaireRead.php" class="btn btn-outline-info">Gestionnaire</a>
+            <a href="../crudGestionnaire/gestionnaireRead.php" class="btn btn-outline-info active">Gestionnaire</a>
             <a href="../crudOffre/offreRead.php" class="btn btn-outline-info">Offre</a>
             <a href="../crudPartenaire/partenaireRead.php" class="btn btn-outline-info">Partenaire</a>
             <a href="../crudPost/postRead.php" class="btn btn-outline-danger disabled">Post</a>
@@ -122,5 +142,6 @@ $utilisateurs = $utilisateurRepo->findNonGestionnaires();
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
